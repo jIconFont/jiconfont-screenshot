0f659fea052b72e645b51e06ea926a427189b539
Carlos Eduardo Leite de Andrade <cadu@caduandrade.net>
2016-02-11 09:38:09 -0200

    Adding project files

--- src/main/java/jiconfont/screenshot/IconFontScreenshot.java
+++ src/main/java/jiconfont/screenshot/IconFontScreenshot.java
@@ -1,25 +1,33 @@
 package jiconfont.screenshot;
 
+import java.io.File;
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.SnapshotParameters;
-import javafx.scene.control.Label;
 import javafx.scene.image.WritableImage;
-import javafx.scene.layout.*;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import jiconfont.IconCode;
-import jiconfont.icons.*;
-import jiconfont.javafx.IconFontFX;
-import jiconfont.javafx.IconNode;
 
 import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
 
+import jiconfont.IconCode;
+import jiconfont.icons.Elusive;
+import jiconfont.icons.Entypo;
+import jiconfont.icons.FontAwesome;
+import jiconfont.icons.GoogleMaterialDesignIcons;
+import jiconfont.icons.Iconic;
+import jiconfont.javafx.IconFontFX;
+import jiconfont.javafx.IconNode;
 
 /**
  * Copyright (c) 2016 jIconFont <BR>
@@ -44,110 +52,156 @@ import java.io.IOException;
  */
 public class IconFontScreenshot extends Application {
 
-    private void writeImage(Pane iconsPane, String filename) {
-        filename = "./target/" + filename.toLowerCase() + ".png";
-        new Scene(iconsPane);
-        WritableImage image = iconsPane.snapshot(new SnapshotParameters(), null);
-        File file = new File(filename);
-        try {
-            ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+  private void writeImage(Pane iconsPane, String path, String filename) {
+    String f = "./target/";
+    if (path != null) {
+      f += path.toLowerCase() + "/";
+      File dir = new File(f);
+      if (dir.exists() == false) {
+        dir.mkdir();
+      }
+    }
+    f += filename.toLowerCase() + ".png";
+    new Scene(iconsPane);
+    WritableImage image = iconsPane.snapshot(new SnapshotParameters(), null);
+    File file = new File(f);
+    try {
+      ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private IconNode buildIconNode(IconCode iconCode, Number size, Color fill) {
+    IconNode iconNode = new IconNode(iconCode);
+    iconNode.setIconSize(size);
+    iconNode.setFill(fill);
+    return iconNode;
+  }
+
+  private void iconScreenshot(IconCode iconCode, String filename) {
+    HBox iconsPane = new HBox();
+    iconsPane.setAlignment(Pos.CENTER);
+    iconsPane.setBackground(
+      new Background(new BackgroundFill(Color.WHITE, null, null)));
+
+    IconNode iconNode = buildIconNode(iconCode, 25, Color.CORNFLOWERBLUE);
+    iconsPane.getChildren().add(iconNode);
+
+    writeImage(iconsPane, null, filename);
+  }
+
+  private void createMultipleColors(IconCode iconCode, String filename) {
+    HBox iconsPane = new HBox();
+    iconsPane.setAlignment(Pos.CENTER);
+    iconsPane.setSpacing(5);
+    iconsPane.setBackground(
+      new Background(new BackgroundFill(Color.WHITE, null, null)));
+
+    IconNode iconNode1 = buildIconNode(iconCode, 30, Color.CORNFLOWERBLUE);
+    iconsPane.getChildren().add(iconNode1);
+
+    IconNode iconNode2 = buildIconNode(iconCode, 30, Color.DEEPSKYBLUE);
+    iconsPane.getChildren().add(iconNode2);
+
+    IconNode iconNode3 = buildIconNode(iconCode, 30, Color.DARKTURQUOISE);
+    iconsPane.getChildren().add(iconNode3);
+
+    IconNode iconNode4 = buildIconNode(iconCode, 30, Color.LIGHTBLUE);
+    iconsPane.getChildren().add(iconNode4);
+
+    writeImage(iconsPane, null, filename);
+  }
+
+  private void createMultipleSizes(IconCode iconCode, String filename) {
+    HBox iconsPane = new HBox();
+    iconsPane.setAlignment(Pos.CENTER);
+    iconsPane.setSpacing(5);
+    iconsPane.setBackground(
+      new Background(new BackgroundFill(Color.WHITE, null, null)));
+
+    int size = 10;
+    for (int i = 0; i < 5; i++) {
+      IconNode iconNode = buildIconNode(iconCode, size, Color.BLACK);
+      iconsPane.getChildren().add(iconNode);
+      size = size + 10;
     }
 
-    private IconNode buildIconNode(IconCode iconCode, Number size, Color fill) {
-        IconNode iconNode = new IconNode(iconCode);
-        iconNode.setIconSize(size);
-        iconNode.setFill(fill);
-       return iconNode;
+    writeImage(iconsPane, null, filename);
+  }
+
+  private void iconFontScreenshot(IconCode[] iconCodes, String filename) {
+    TilePane iconsPane = new TilePane();
+    iconsPane.setAlignment(Pos.CENTER);
+    iconsPane.setPrefWidth(650);
+    iconsPane.setMaxWidth(650);
+    iconsPane.setBackground(
+      new Background(new BackgroundFill(Color.WHITE, null, null)));
+    iconsPane.setHgap(5);
+    iconsPane.setVgap(5);
+    for (IconCode icon : iconCodes) {
+      IconNode iconNode = buildIconNode(icon, 25, Color.BLACK);
+      iconsPane.getChildren().add(iconNode);
     }
+    writeImage(iconsPane, null, filename);
+  }
 
-    private void createIcon(IconCode iconCode, String filename) {
-        HBox iconsPane = new HBox();
-        iconsPane.setAlignment(Pos.CENTER);
-        iconsPane.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
-
-        IconNode iconNode = buildIconNode(iconCode, 25, Color.CORNFLOWERBLUE);
-        iconsPane.getChildren().add(iconNode);
-
-        writeImage(iconsPane, filename);
+  private void iconCatalogScreenshot(IconCode[] iconCodes, String path) {
+    for (IconCode icon : iconCodes) {
+      TilePane iconsPane = new TilePane();
+      iconsPane.setAlignment(Pos.CENTER);
+      iconsPane.setPrefWidth(150);
+      iconsPane.setMaxWidth(150);
+      iconsPane.setBackground(
+        new Background(new BackgroundFill(Color.WHITE, null, null)));
+      iconsPane.setHgap(5);
+      iconsPane.setVgap(5);
+      IconNode iconNode1 = buildIconNode(icon, 16, Color.BLACK);
+      IconNode iconNode2 = buildIconNode(icon, 20, Color.BLACK);
+      IconNode iconNode3 = buildIconNode(icon, 24, Color.BLACK);
+      IconNode iconNode4 = buildIconNode(icon, 32, Color.BLACK);
+      iconsPane.getChildren().addAll(iconNode1, iconNode2, iconNode3,
+        iconNode4);
+      String filename = icon.name().toLowerCase();
+      writeImage(iconsPane, path, filename);
     }
+  }
 
-    private void createMultipleColors(IconCode iconCode, String filename) {
-        HBox iconsPane = new HBox();
-        iconsPane.setAlignment(Pos.CENTER);
-        iconsPane.setSpacing(5);
-        iconsPane.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+  @Override
+  public void start(Stage stage) {
+    IconFontFX.register(GoogleMaterialDesignIcons.getIconFont());
+    IconFontFX.register(FontAwesome.getIconFont());
+    IconFontFX.register(Iconic.getIconFont());
+    IconFontFX.register(Elusive.getIconFont());
+    IconFontFX.register(Entypo.getIconFont());
 
-        IconNode iconNode1 = buildIconNode(iconCode, 30, Color.CORNFLOWERBLUE);
-        iconsPane.getChildren().add(iconNode1);
+    iconFontScreenshot(GoogleMaterialDesignIcons.values(),
+      GoogleMaterialDesignIcons.class.getSimpleName());
+    iconFontScreenshot(FontAwesome.values(), FontAwesome.class.getSimpleName());
+    iconFontScreenshot(Iconic.values(), Iconic.class.getSimpleName());
+    iconFontScreenshot(Elusive.values(), Elusive.class.getSimpleName());
+    iconFontScreenshot(Entypo.values(), Entypo.class.getSimpleName());
 
-        IconNode iconNode2 = buildIconNode(iconCode, 30, Color.DEEPSKYBLUE);
-        iconsPane.getChildren().add(iconNode2);
+    iconCatalogScreenshot(GoogleMaterialDesignIcons.values(),
+      GoogleMaterialDesignIcons.class.getSimpleName());
+    iconCatalogScreenshot(FontAwesome.values(),
+      FontAwesome.class.getSimpleName());
+    iconCatalogScreenshot(Iconic.values(), Iconic.class.getSimpleName());
+    iconCatalogScreenshot(Elusive.values(), Elusive.class.getSimpleName());
+    iconCatalogScreenshot(Entypo.values(), Entypo.class.getSimpleName());
 
-        IconNode iconNode3 = buildIconNode(iconCode, 30, Color.DARKTURQUOISE);
-        iconsPane.getChildren().add(iconNode3);
+    createMultipleSizes(FontAwesome.CHECK, "multiplesizes");
 
-        IconNode iconNode4 = buildIconNode(iconCode, 30, Color.LIGHTBLUE);
-        iconsPane.getChildren().add(iconNode4);
+    createMultipleColors(FontAwesome.CHECK, "multiplecolors");
 
-        writeImage(iconsPane, filename);
-    }
+    iconScreenshot(Elusive.LINKEDIN, "linkedin");
+    iconScreenshot(Elusive.GITHUB, "github");
 
-    private void createMultipleSizes(IconCode iconCode, String filename) {
-        HBox iconsPane = new HBox();
-        iconsPane.setAlignment(Pos.CENTER);
-        iconsPane.setSpacing(5);
-        iconsPane.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+    Platform.exit();
+  }
 
-        int size = 10;
-        for (int i = 0; i < 5; i++) {
-            IconNode iconNode = buildIconNode(iconCode, size, Color.BLACK);
-            iconsPane.getChildren().add(iconNode);
-            size = size + 10;
-        }
-
-        writeImage(iconsPane, filename);
-    }
-
-    private void createIcon(IconCode[] iconCodes, String filename) {
-        TilePane iconsPane = new TilePane();
-        iconsPane.setAlignment(Pos.CENTER);
-        iconsPane.setPrefWidth(650);
-        iconsPane.setMaxWidth(650);
-        iconsPane.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
-        iconsPane.setHgap(5);
-        iconsPane.setVgap(5);
-        for (IconCode icon : iconCodes) {
-            IconNode iconNode = buildIconNode(icon, 25, Color.BLACK);
-            iconsPane.getChildren().add(iconNode);
-        }
-
-        writeImage(iconsPane, filename);
-    }
-
-    @Override
-    public void start(Stage stage) {
-        IconFontFX.register(GoogleMaterialDesignIcons.getIconFont());
-        IconFontFX.register(FontAwesome.getIconFont());
-        IconFontFX.register(Iconic.getIconFont());
-        IconFontFX.register(Elusive.getIconFont());
-        IconFontFX.register(Entypo.getIconFont());
-
-        createIcon(GoogleMaterialDesignIcons.values(), GoogleMaterialDesignIcons.class.getSimpleName());
-        createIcon(FontAwesome.values(), FontAwesome.class.getSimpleName());
-        createIcon(Iconic.values(), Iconic.class.getSimpleName());
-        createIcon(Elusive.values(), Elusive.class.getSimpleName());
-        createIcon(Entypo.values(), Entypo.class.getSimpleName());
-        createMultipleSizes(FontAwesome.CHECK, "multiplesizes");
-        createMultipleColors(FontAwesome.CHECK, "multiplecolors");
-        createIcon(Elusive.LINKEDIN, "linkedin");
-        createIcon(Elusive.GITHUB, "github");
-        Platform.exit();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
